1453cb6d4867e8cc990829b7a9a5feb7e690e2af
meeraunni <167728534+meeraunni@users.noreply.github.com>
2026-03-12 21:45:47 -0400

    Update post-header.tsx

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -1,33 +1,38 @@
 import Avatar from "./avatar";
 import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
-import { PostTitle } from "@/app/_components/post-title";
-import { type Author } from "@/interfaces/author";
+import PostTitle from "./post-title";
 
 type Props = {
   title: string;
   coverImage: string;
   date: string;
-  author: Author;
+  author: {
+    name: string;
+    picture: string;
+  };
 };
 
-export function PostHeader({ title, coverImage, date, author }: Props) {
+export default function PostHeader({
+  title,
+  coverImage,
+  date,
+  author,
+}: Props) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
+
+      <div className="mb-6">
+        <Avatar name={author?.name || "Sentinel Identity"} />
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
+
+      <div className="mb-6 text-sm text-neutral-600">
+        <DateFormatter dateString={date} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
+
+      <div className="mb-10">
+        <CoverImage title={title} src={coverImage} priority />
       </div>
     </>
   );

c27a4ccd230771cad535157a01eac8367afc511a
meeraunni <167728534+meeraunni@users.noreply.github.com>
2026-03-21 23:57:07 -0400

    Update container.tsx

--- src/app/_components/container.tsx
+++ src/app/_components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className="container mx-auto px-5">{children}</div>;
+  return <div className="mx-auto w-full max-w-7xl px-6 lg:px-10">{children}</div>;
 };
 
 export default Container;
